f4ed52999aa814c7e4361d3edad19917d96cb9ae
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2020-05-02 21:00:04 +0200

    Added errormessages in html for form

--- huizenapp/src/app/huis/add-huis/add-huis.component.ts
+++ huizenapp/src/app/huis/add-huis/add-huis.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, ValidatorFn } from '@angular/forms';
 import { Huis } from '../huis/huis.model';
 import { Locatie } from '../locatie/locatie.model';
 import { Detail } from 'src/app/detail/detail.model';
@@ -13,6 +13,7 @@ import { Observable } from 'rxjs';
   templateUrl: './add-huis.component.html',
   styleUrls: ['./add-huis.component.css']
 })
+  
 export class AddHuisComponent implements OnInit {
   private _fetchBureaus$: Observable<ImmoBureau[]>;
   public locatie: FormGroup;
@@ -26,29 +27,27 @@ export class AddHuisComponent implements OnInit {
   public readonly types = ['Koop', 'Huur'];
   public readonly soorten = ['Huis', 'Appartement', 'Grond'];
 
-  //Temporary solution for getting immobureaus from API is there are doubles in the api.. NgFor in mat selector shows doubles..
-  // public readonly bureaus = ['CD-Vastgoed','Immo Da Vinci' , 'Immo Nobels'];
-
   get bureaus$(): Observable<ImmoBureau[]>{
     return this._fetchBureaus$;
   }
 
   ngOnInit(): void {
     this._fetchBureaus$ = this.bds.immoBureaus$;
+
     this.locatie = this.fb.group({
       gemeente: ['', [Validators.required, Validators.minLength(2)]],
       huisnummer: ['', [Validators.required, Validators.minLength(1)]],
-      postcode: ['', [Validators.required, Validators.min(1000)]],
+      postcode: ['', [Validators.required, , Validators.max(9999), Validators.min(1000)]],
       straatnaam: ['', Validators.required, Validators.minLength(3)]
     });
 
     this.detail = this.fb.group({
       langeBeschrijving: ['', Validators.required, Validators.minLength(30)],
       bewoonbareOppervlakte: ['', Validators.required, Validators.min(15)],
-      totaleOppervlakte: ['', Validators.required, Validators.min(20)],
+      totaleOppervlakte: ['', Validators.required, Validators.min(20), [greaterThanValidator]],
       epcWaarde: ['', Validators.required, Validators.min(0), Validators.max(700)],
       kadastraalInkomen: ['', Validators.required, Validators.min(100)]
-    })
+    });
 
     this.huis = this.fb.group({
       korteBeschrijving: ['', Validators.required, Validators.minLength(10), Validators.maxLength(200)],
@@ -58,6 +57,8 @@ export class AddHuisComponent implements OnInit {
       immoBureau: ['', Validators.required]
     })    
   }
+
+
 
   onSubmit(){    
       this.hds.addNewHuis(new Huis(
@@ -72,15 +73,29 @@ export class AddHuisComponent implements OnInit {
   }
 
   getErrorMessage(errors: any): string {
-    if (!errors) {
-      return null;
+     if (errors.required) {
+       return 'Veld is verplicht';
+     } else if (errors.minlength) {
+       return `Veld moet minstens ${errors.minlength.requiredLength} 
+         karakters lang zijn (is ${errors.minlength.actualLength})`;    
+    }else if(errors.maxLength){
+      return `Veld mag maximaal ${errors.maxLength.requiredLength} lang zijn (heeft ${errors.maxLength.actualLength})`
     }
-    if (errors.required) {
-      return 'is required';
-    } else if (errors.minlength) {
-      return `needs at least ${errors.minlength.requiredLength} characters (got ${errors.minlength.actualLength})`;
-    } else if (errors.amountNoName) {
-      return `if amount is set you must set a name`;
+    else if(errors.min){
+      return `Veld moet groter zijn dan huidige`
+    }
+    else if(errors.max){
+      return `Veld moet kleiner zijn dan huidige`
+    }
+    else if(errors.greaterThan){
+      return `Totale opp. moest minstens bewoonbare opp. zijn`;
     }
   }
+}
+
+function greaterThanValidator(control: FormGroup) : { [key: string] : any } {
+  if(control.get('totaleOppervlakte').value < control.get('bewoonbareOppervlakte').value){
+    return {greaterThan: true};
+  }
+  return null;
 }
