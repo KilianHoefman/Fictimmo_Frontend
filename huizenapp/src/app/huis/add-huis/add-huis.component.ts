77957b020324d00208f0f6d991a89ca9cf22df68
Kilian Hoefman <kilian.hoefman@student.hogent.be>
2020-05-03 11:27:27 +0200

    BugFix form

--- huizenapp/src/app/huis/add-huis/add-huis.component.ts
+++ huizenapp/src/app/huis/add-huis/add-huis.component.ts
@@ -21,11 +21,12 @@ export class AddHuisComponent implements OnInit {
   public huis: FormGroup;
   
 
-  constructor(private fb: FormBuilder, private hds: HuisDataService, private bds: ImmoBureauDataService) { 
+  constructor(private fb: FormBuilder, private _huisDataService: HuisDataService, private bds: ImmoBureauDataService) { 
   }
 
   public readonly types = ['Koop', 'Huur'];
   public readonly soorten = ['Huis', 'Appartement', 'Grond'];
+  //private readonly bureaus = ['CD Vastgoed', 'Immo Da Vinci', 'Immo Nobels'];
 
   get bureaus$(): Observable<ImmoBureau[]>{
     return this._fetchBureaus$;
@@ -37,22 +38,22 @@ export class AddHuisComponent implements OnInit {
     this.locatie = this.fb.group({
       gemeente: ['', [Validators.required, Validators.minLength(2)]],
       huisnummer: ['', [Validators.required, Validators.minLength(1)]],
-      postcode: ['', [Validators.required, , Validators.max(9999), Validators.min(1000)]],
-      straatnaam: ['', Validators.required, Validators.minLength(3)]
+      postcode: ['', [Validators.required, Validators.min(1000), Validators.max(9999)]],
+      straatnaam: ['', [Validators.required, Validators.minLength(3), Validators.max(50)]]
     });
 
     this.detail = this.fb.group({
-      langeBeschrijving: ['', Validators.required, Validators.minLength(30)],
-      bewoonbareOppervlakte: ['', Validators.required, Validators.min(15)],
-      totaleOppervlakte: ['', Validators.required, Validators.min(20), [greaterThanValidator]],
-      epcWaarde: ['', Validators.required, Validators.min(0), Validators.max(700)],
-      kadastraalInkomen: ['', Validators.required, Validators.min(100)]
+      langeBeschrijving: ['', [Validators.required, Validators.minLength(1), Validators.minLength(100)]],
+      bewoonbareOppervlakte: ['', [Validators.required, Validators.min(1)]],
+      totaleOppervlakte: ['', [Validators.required, Validators.min(1)]],
+      epcWaarde: ['', [Validators.required, Validators.min(0), Validators.max(900)]],
+      kadastraalInkomen: ['', [Validators.required, Validators.min(1)]]
     });
 
     this.huis = this.fb.group({
-      korteBeschrijving: ['', Validators.required, Validators.minLength(10), Validators.maxLength(200)],
-      price: ['', Validators.required],
-      type: ['', Validators.required],
+      korteBeschrijving: ['', [Validators.required, Validators.maxLength(250)]],
+      price: ['', [Validators.required, Validators.min(1)]],
+    type: ['', Validators.required],
       soort: ['', Validators.required],
       immoBureau: ['', Validators.required]
     })    
@@ -61,7 +62,7 @@ export class AddHuisComponent implements OnInit {
 
 
   onSubmit(){    
-      this.hds.addNewHuis(new Huis(
+      this._huisDataService.addNewHuis(new Huis(
       new Locatie(this.locatie.value.gemeente, this.locatie.value.straatnaam, this.locatie.value.huisnummer, this.locatie.value.postcode), 
       this.huis.value.korteBeschrijving, 
       this.huis.value.price, 
@@ -93,9 +94,9 @@ export class AddHuisComponent implements OnInit {
   }
 }
 
-function greaterThanValidator(control: FormGroup) : { [key: string] : any } {
-  if(control.get('totaleOppervlakte').value < control.get('bewoonbareOppervlakte').value){
-    return {greaterThan: true};
-  }
-  return null;
-}
+// function greaterThanValidator(control: FormGroup) : { [key: string] : any } {
+//   if(control.get('totaleOppervlakte').value < control.get('bewoonbareOppervlakte').value){
+//     return {greaterThan: true};
+//   }
+//   return null;
+// }
